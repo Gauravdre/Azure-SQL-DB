fcae06d3e9b1872f05a713f91911c0f05174f662
Gaurav T <gauravdre@gmail.com>
2023-03-06 07:14:24 +0530

    Where with Filter

--- SET_2.sql
+++ SET_2.sql
@@ -1,4 +1,4 @@
-use AdventureWorksDW2014;
+use AdventureWorksDW2019;
 
 
 -- inner join
@@ -20,3 +20,41 @@ from FactInternetSales s
 inner join DimProduct p 
 	on	s.ProductKey = p.ProductKey 
 	and	p.StartDate > '2013-01-01'
+	
+-- basic filter with WHERE
+-- get sales of a specific product only
+SELECT *
+FROM FactInternetSales s
+INNER JOIN DimProduct p ON s.ProductKey = p.ProductKey
+WHERE p.EnglishProductName = 'Road-650 Black, 62'
+
+-- non-equi-filters
+-- get all orders for 2013
+SELECT *
+FROM FactInternetSales s
+INNER JOIN DimProduct p ON s.ProductKey = p.ProductKey
+WHERE	s.OrderDate >= '2013-01-01'
+AND		s.OrderDate <= '2013-12-31'
+
+-- also can use "between" for dates
+SELECT *
+FROM FactInternetSales s
+INNER JOIN DimProduct p ON s.ProductKey = p.ProductKey
+WHERE s.OrderDate BETWEEN '2013-01-01' AND '2013-12-31';
+
+-- filter for multiple values using IN
+SELECT *
+FROM FactInternetSales s
+INNER JOIN DimProduct p ON s.ProductKey = p.ProductKey
+WHERE p.EnglishProductName in( 
+		'Road-150 Red, 48',
+		'Road-150 Red, 52',
+		'Road-150 Red, 62',
+		'Road-150 Red, 56')
+
+
+-- find all current and future matches with LIKE
+SELECT *
+FROM FactInternetSales s
+INNER JOIN DimProduct p ON s.ProductKey = p.ProductKey
+WHERE p.EnglishProductName LIKE 'Road%' --put % where you want wildcard
